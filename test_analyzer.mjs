42debab6a0f6363d41deb8052615969b8887821b
Mohammed Awad <procoder.main@gmail.com>
2026-09-11 18:42:56 +0300

    Untrack test and fix scripts from repository

--- test_analyzer.mjs
+++
@@ -1,7 +0,0 @@
-import { analyzeAndProcessScripts } from './server/services/scriptAnalyzer.js';
-
-const html = `<script type="module" src="../../_assets/b7e754b6fcef9bc3.js"></script>`;
-const processed = analyzeAndProcessScripts(html);
-console.log("PROCESSED HTML:");
-console.log(processed);
-
--- test_html.mjs
+++
@@ -1,9 +0,0 @@
-import * as cheerio from 'cheerio';
-import fs from 'fs';
-
-const html = fs.readFileSync('clones/5c9ed101-5210-428b-88cd-5ae3d4f89a10/index.html', 'utf8');
-const $ = cheerio.load(html);
-
-$('video').each((i, el) => {
-  console.log($(el).attr('src') || 'no-src', 'muted:', $(el).attr('muted'));
-});
